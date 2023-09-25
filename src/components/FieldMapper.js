7e24760e4092b3931fee3df1fe151d1cb2df3bf4
avielite <avish@elitefit.ai>
2023-09-26 01:04:36 +0530

    fixed changes

--- src/components/FieldMapper.js
+++ src/components/FieldMapper.js
@@ -7,8 +7,8 @@ class FieldMapper extends Component {
     super(props);
 
     this.state = {
-      leftFields: ['Field 1', 'Field 2', 'Field 3'], // Replace with your Salesforce fields
-      rightFields: ['Field A', 'Field B', 'Field C'], // Initialize with an empty array for CallHub fields
+      leftFields: ['First Name', 'Last Name', 'Contact ID', 'Master Record ID ', 'Account ID', 'Salutation', 'Street', 'City', 'State/Provinces', 'Zip/Postal Code', 'End_date', 'Frequency_of_giving', 'Ward'], // Replace with your Salesforce fields
+      rightFields: ['First Name', 'Last Name', 'Contact ID', 'Master Record ID ', 'Account ID', 'Salutation', 'Street', 'City', 'State/Provinces', 'Zip/Postal Code', 'End_date', 'Frequency_of_giving', 'Ward'], // Initialize with an empty array for CallHub fields
       mappings: [{}], // Initialize with an empty mapping array
       addedInputs: [], // This array is being used for disabling the used fields
       selectedInput1: {}, // this array is being used for storing left data points 
@@ -32,19 +32,30 @@ class FieldMapper extends Component {
           this.state.selectedInput2[index] = value;
         }
       }
+    this.setState(this.state.selectedInput1);
+    this.setState(this.state.selectedInput2);
   };
 
   handleMappingDelete = (index) => {
     // Clone the current mappings array
     const mappings = [...this.state.mappings];
 
-    // Add Back removed data in left and right fields
-    this.state.addedInputs.filter((item) => item !== mappings[index].left);
-    this.state.addedInputs.filter((item) => item !== mappings[index].right);
+    console.log(mappings[index].left, mappings[index].right)
+
+    const addedInput = this.state.addedInputs;
+
+    console.log(addedInput);
+
+    let leftItemIndex = addedInput.indexOf(mappings[index].left);
+    addedInput.splice(leftItemIndex, 1);
+
+    let rightItemIndex = addedInput.indexOf(mappings[index].right);
+    addedInput.splice(rightItemIndex, 1);
+
+    this.setState(addedInput);
 
     // Remove the mapping at the specified index
     mappings.splice(index, 1);
-  
 
     // Update the state
     this.setState({ mappings });
@@ -68,10 +79,13 @@ class FieldMapper extends Component {
           let selected2 = this.state.selectedInput2[iterator];
           if(selected1 != '' && selected2 != '' && this.state.selectedInput1.index == this.state.selectedInput2.index) {
             this.state.mappings.push({left: selected1, right: selected2});
-            this.state.addedInputs.push(selected1);
-            this.state.addedInputs.push(selected2);
-            this.setState(this.state.addedInputs);
-            this.setState(this.state.mappings);
+            if(!this.state.addedInputs.includes(selected1)) {
+              this.state.addedInputs.push(selected1);
+              this.state.addedInputs.push(selected2);
+              console.log(this.state.addedInputs);
+              this.setState(this.state.addedInputs);
+              this.setState(this.state.mappings);
+            }
             this.handleAddMapping();
           }
           else {
@@ -95,39 +109,46 @@ class FieldMapper extends Component {
       <div class="container">
         <h2 class="header">Field Mapper</h2>
         <div class="row">
-          <div class="col-5">
+          <div class="col-7">
             <h3>Salesforce</h3>
             {mappings.map((mapping, index) => (
               <div key={index}>
-                <select class="custom-select"
+                <p>{this.state.selectedInput1[index]}</p>
+                <select class="custom-select1"
                   value={mapping.left || ''}
                   onChange={(e) => this.handleMappingChange(index, 'left', e.target.value)}>
                   <option value="">Select Field</option>
-                  {leftFields.map((field) => (
-                    this.state.addedInputs.includes(field) ? <option disabled key={field} value={field}>{field}</option> : <option key={field} value={field}>{field}</option>
-                  ))}
+                    {leftFields.map((field) => (
+                      this.state.addedInputs.includes(field) ? <option disabled key={field} value={field}>{field}</option> : <option selected key={field} value={field}>{field}</option>
+                    ))}
                 </select>
                 </div>
               ))}
           </div>
           <div class="col-5">
-          <h3>CallHub</h3>
+            <h3 class="col-8">CallHub</h3>
           {mappings.map((mapping, index) => (
           <div key={index}>
-            <select class="custom-select dropdown2"
-              value={mapping.right || ''}
-              onChange={(e) => this.handleMappingChange(index, 'right', e.target.value)}>
-              <option value="">Select Field</option>
-              {rightFields.map((field) => (
-                this.state.addedInputs.includes(field) ? <option disabled key={field} value={field}>{field}</option> : <option key={field} value={field}>{field}</option>
-              ))}
-            </select>
-            <div class="col-5 dlt-btn">
-              <i class="bi bi-trash" style={{fontSize: 1.5 + 'em'}} onClick={() => this.handleMappingDelete(index)}></i>
-            </div>
+            <p class="col-7">{this.state.selectedInput2[index]}</p>
+            <table> 
+              <td>
+              <select class="custom-select2"
+                value={mapping.right || ''}
+                onChange={(e) => this.handleMappingChange(index, 'right', e.target.value)}>
+                <option value="">Select Field</option>
+                  {rightFields.map((field) => (
+                    this.state.addedInputs.includes(field) ? <option disabled key={field} value={field}>{field}</option> : <option selected key={field} value={field}>{field}</option>
+                  ))}
+              </select>
+              </td>
+              <td>
+                <div class="dlt-btn">
+                  <i class="bi bi-trash" style={{fontSize: 1.5 + 'em'}} onClick={() => this.handleMappingDelete(index)}></i>
+                </div>
+              </td>
+            </table>
           </div>           
           ))}
-          
         </div>
           <button class="btn btn-success mb-4 w-3" onClick={this.handleAddMapping}>Add Mapping</button>
           <button class="btn btn-success" onClick={this.handleSubmit}>Submit</button>
