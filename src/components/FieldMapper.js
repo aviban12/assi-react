2b355dbb522dfdec0e733dddbd3b67cda77141ce
Avish <36235379+aviban12@users.noreply.github.com>
2023-09-26 17:27:50 +0000

    Update package-lock.json and FieldMapper.js

--- src/components/FieldMapper.js
+++ src/components/FieldMapper.js
@@ -1,38 +1,72 @@
-import React, { Component } from 'react';
-import './FieldMapper.css';
-import MappedFieldsTable from './MappedFieldsTable';
+import React, { Component } from "react";
+import "./FieldMapper.css";
+import MappedFieldsTable from "./MappedFieldsTable";
 
 class FieldMapper extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      leftFields: ['First Name', 'Last Name', 'Contact ID', 'Master Record ID ', 'Account ID', 'Salutation', 'Street', 'City', 'State/Provinces', 'Zip/Postal Code', 'End_date', 'Frequency_of_giving', 'Ward'], // Replace with your Salesforce fields
-      rightFields: ['First Name', 'Last Name', 'Contact ID', 'Master Record ID ', 'Account ID', 'Salutation', 'Street', 'City', 'State/Provinces', 'Zip/Postal Code', 'End_date', 'Frequency_of_giving', 'Ward'], // Initialize with an empty array for CallHub fields
+      leftFields: [
+        "First Name",
+        "Last Name",
+        "Contact ID",
+        "Master Record ID ",
+        "Account ID",
+        "Salutation",
+        "Street",
+        "City",
+        "State/Provinces",
+        "Zip/Postal Code",
+        "End_date",
+        "Frequency_of_giving",
+        "Ward",
+      ], // Replace with your Salesforce fields
+      rightFields: [
+        "First Name",
+        "Last Name",
+        "Contact ID",
+        "Master Record ID ",
+        "Account ID",
+        "Salutation",
+        "Street",
+        "City",
+        "State/Provinces",
+        "Zip/Postal Code",
+        "End_date",
+        "Frequency_of_giving",
+        "Ward",
+      ], // Initialize with an empty array for CallHub fields
       mappings: [{}], // Initialize with an empty mapping array
       addedInputs1: [], // This array is being used for disabling the used fields
       addedInputs2: [],
-      selectedInput1: {}, // this array is being used for storing left data points 
-      selectedInput2: {},// this array is being used for storing right data points
+      selectedInput1: {}, // this array is being used for storing left data points
+      selectedInput2: {}, // this array is being used for storing right data points
     };
   }
 
   handleMappingChange = (index, side, value) => {
     // Insert data in a new object based on index and right and left
-    if(side == 'left') {
-      if(this.state.selectedInput1[index] == undefined) {
+    if (side === "left") {
+      if (this.state.selectedInput1[index] == undefined) {
         this.state.selectedInput1[index] = value;
       } else {
         this.state.selectedInput1[index] = value;
       }
+      let leftMappings = this.state.leftFields;
+      let indexOfLeft = leftMappings.indexOf(value);
+      leftMappings.splice(indexOfLeft, 1);
     }
-    if(side == 'right') {
-        if(this.state.selectedInput2[index] == undefined) {
-          this.state.selectedInput2[index] = value;
-        }else {
-          this.state.selectedInput2[index] = value;
-        }
+    if (side === "right") {
+      if (this.state.selectedInput2[index] === undefined) {
+        this.state.selectedInput2[index] = value;
+      } else {
+        this.state.selectedInput2[index] = value;
       }
+      let rightMappings = this.state.rightFields;
+      let indexOfRight = rightMappings.indexOf(value);
+      rightMappings.splice(indexOfRight, 1);
+    }
     this.setState(this.state.selectedInput1);
     this.setState(this.state.selectedInput2);
   };
@@ -43,14 +77,19 @@ class FieldMapper extends Component {
 
     const leftAddedInput = this.state.addedInputs1;
     const rightAddedInput = this.state.addedInputs2;
-
+    let leftMappings = this.state.leftFields;
+    let rightMappings = this.state.rightFields;
 
     let leftItemIndex = leftAddedInput.indexOf(mappings[index].left);
+    leftMappings.push(mappings[index].left);
     leftAddedInput.splice(leftItemIndex, 1);
 
     let rightItemIndex = rightAddedInput.indexOf(mappings[index].right);
     rightAddedInput.splice(rightItemIndex, 1);
+    rightMappings.push(mappings[index].right);
 
+    this.setState(leftMappings);
+    this.setState(rightMappings);
     this.setState(leftAddedInput);
     this.setState(rightAddedInput);
 
@@ -64,7 +103,7 @@ class FieldMapper extends Component {
   handleAddMapping = () => {
     // Clone the current mappings array and add an empty mapping
     const mappings = [...this.state.mappings, {}];
-    
+
     // Update the state
     this.setState({ mappings });
   };
@@ -72,36 +111,37 @@ class FieldMapper extends Component {
   handleSubmit = () => {
     let obj1len = Object.keys(this.state.selectedInput1).length;
     let obj2len = Object.keys(this.state.selectedInput2).length;
-    if(obj1len == obj2len) {
+    if (obj1len === obj2len) {
       this.state.mappings = [];
-      for(let iterator=0; iterator < obj1len; iterator++) {
-          let selected1 = this.state.selectedInput1[iterator];
-          let selected2 = this.state.selectedInput2[iterator];
-          if(selected1 != '' && selected2 != '' && this.state.selectedInput1.index == this.state.selectedInput2.index) {
-            this.state.mappings.push({left: selected1, right: selected2});
-            if(!this.state.addedInputs1.includes(selected1)) {
-              this.state.addedInputs1.push(selected1);
-            }
-            if(!this.state.addedInputs2.includes(selected2)) {
-                this.state.addedInputs2.push(selected2);
-              }
-            this.setState(this.state.addedInputs1);
-            this.setState(this.state.addedInputs2);
-            this.setState(this.state.mappings);
+      for (let iterator = 0; iterator < obj1len; iterator++) {
+        let selected1 = this.state.selectedInput1[iterator];
+        let selected2 = this.state.selectedInput2[iterator];
+        if (
+          selected1 !== "" &&
+          selected2 !== "" &&
+          this.state.selectedInput1.index === this.state.selectedInput2.index
+        ) {
+          this.state.mappings.push({ left: selected1, right: selected2 });
+          if (!this.state.addedInputs1.includes(selected1)) {
+            this.state.addedInputs1.push(selected1);
           }
-          else {
-            console.log('Index Count Mismatched');
+          if (!this.state.addedInputs2.includes(selected2)) {
+            this.state.addedInputs2.push(selected2);
           }
-          this.state.selectedInput1.value = '';
-          this.state.selectedInput2.value = '';
-          this.setState(this.state.selectedInput1);
-          this.setState(this.state.selectedInput2);
+          this.setState(this.state.addedInputs1);
+          this.setState(this.state.addedInputs2);
+          this.setState(this.state.mappings);
+        } else {
+          console.log("Index Count Mismatched");
         }
-        this.handleAddMapping();
+        this.state.selectedInput1.value = "";
+        this.state.selectedInput2.value = "";
+        this.setState(this.state.selectedInput1);
+        this.setState(this.state.selectedInput2);
       }
-      else {
-        alert('Please select both the field in each row');
-      }
+    } else {
+      alert("Please select both the field in each row");
+    }
   };
 
   render() {
@@ -116,48 +156,83 @@ class FieldMapper extends Component {
             {mappings.map((mapping, index) => (
               <div key={index}>
                 <p>{this.state.selectedInput1[index]}</p>
-                <select class="custom-select1"
-                  value={mapping.left || ''}
-                  onChange={(e) => this.handleMappingChange(index, 'left', e.target.value)}>
+                <select
+                  class="custom-select1"
+                  value={mapping.left || ""}
+                  onChange={(e) =>
+                    this.handleMappingChange(index, "left", e.target.value)
+                  }
+                >
                   <option value="">Select Field</option>
-                    {leftFields.map((field) => (
-                      this.state.addedInputs1.includes(field) ? <option disabled key={field} value={field} hidden>{field}</option> : <option selected key={field} value={field}>{field}</option>
-                    ))}
+                  {leftFields.map((field) =>
+                    this.state.selectedInput1[index] === field ? (
+                      <option disabled key={field} value={field} hidden>
+                        {field}
+                      </option>
+                    ) : (
+                      <option selected key={field} value={field}>
+                        {field}
+                      </option>
+                    ),
+                  )}
                 </select>
-                </div>
-              ))}
+              </div>
+            ))}
           </div>
           <div class="col-5">
             <h3 class="col-7">CallHub Custom Fields</h3>
-          {mappings.map((mapping, index) => (
-          <div key={index}>
-            <p class="col-6">{this.state.selectedInput2[index]}</p>
-            <table> 
-              <td>
-              <select class="custom-select2"
-                value={mapping.right || ''}
-                onChange={(e) => this.handleMappingChange(index, 'right', e.target.value)}>
-                <option value="">Select Field</option>
-                  {rightFields.map((field) => (
-                    this.state.addedInputs2.includes(field) ? <option disabled key={field} value={field} hidden>{field}</option> : <option selected key={field} value={field}>{field}</option>
-                  ))}
-              </select>
-              </td>
-              <td>
-                <div class="dlt-btn">
-                  <i class="bi bi-trash" style={{fontSize: 1.5 + 'em'}} onClick={() => this.handleMappingDelete(index)}></i>
-                </div>
-              </td>
-            </table>
-          </div>           
-          ))}
-        </div>
-          <button class="btn ml-4 mb-4 custom-select1 map-btn" onClick={this.handleAddMapping}>Map Another Field</button>
-          <button class="btn submit-btn" onClick={this.handleSubmit}>Submit</button>
+            {mappings.map((mapping, index) => (
+              <div key={index}>
+                <p class="col-6">{this.state.selectedInput2[index]}</p>
+                <table>
+                  <td>
+                    <select
+                      class="custom-select2"
+                      value={mapping.right || ""}
+                      onChange={(e) =>
+                        this.handleMappingChange(index, "right", e.target.value)
+                      }
+                    >
+                      <option value="">Select Field</option>
+                      {rightFields.map((field) =>
+                        this.state.selectedInput2[index] === field ? (
+                          <option disabled key={field} value={field} hidden>
+                            {field}
+                          </option>
+                        ) : (
+                          <option selected key={field} value={field}>
+                            {field}
+                          </option>
+                        ),
+                      )}
+                    </select>
+                  </td>
+                  <td>
+                    <div class="dlt-btn">
+                      <i
+                        class="bi bi-trash"
+                        style={{ fontSize: 1.5 + "em" }}
+                        onClick={() => this.handleMappingDelete(index)}
+                      ></i>
+                    </div>
+                  </td>
+                </table>
+              </div>
+            ))}
+          </div>
+          <button
+            class="btn ml-4 mb-4 custom-select1 map-btn"
+            onClick={this.handleAddMapping}
+          >
+            Map Another Field
+          </button>
+          <button class="btn submit-btn" onClick={this.handleSubmit}>
+            Submit
+          </button>
           <MappedFieldsTable mappings={mappings}></MappedFieldsTable>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 export default FieldMapper;
